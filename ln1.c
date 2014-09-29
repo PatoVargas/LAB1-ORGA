4f3384bc6ccd13fb505170c04149b01cc9476a6f
PatoVargas <patricio.vargas@usach.cl>
2014-09-28 21:23:05 -0300

    Update ln1.c
    cambiamos las divisiones por constantes para que el programa se ejecute mas rapido

---
+++ ln1.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+
+double static a0 = 1.0;
+double static a1 = 0.3333333333333;
+double static a2 = 0.2; 
+double static a3 = 0.1428571428571; 
+double static a4 = 0.1111111111111; 
+double static a5 = 0.090909090909; 
+double static a6 = 0.076923076923; 
