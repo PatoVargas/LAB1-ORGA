45bcd3984f65f9add8d4779b9093dc1ad7ed0e55
PatoVargas <patricio.vargas@usach.cl>
2014-09-28 21:37:41 -0300

    utilizamos la misma funcion ln anterior salvo que cambiamos las divisiones por multiplicaciones

--- ln1.c
+++ ln1.c
@@ -1,12 +1,28 @@
 #include <stdio.h> 
 
-double static a0 = 1.0;
-double static a1 = 0.3333333333333;
-double static a2 = 0.2; 
-double static a3 = 0.1428571428571; 
-double static a4 = 0.1111111111111; 
-double static a5 = 0.090909090909; 
-double static a6 = 0.076923076923;
+double static a0 = 1.0; //para guarda 1
+double static a1 = 0.3333333333333; // para guardar 1/3
+double static a2 = 0.2;             // para guardar 1/5
+double static a3 = 0.1428571428571; // para guardar 1/7
+double static a4 = 0.1111111111111; // para guardar 1/9
+double static a5 = 0.090909090909; // para guardar 1/11
+double static a6 = 0.076923076923; // para guardar 1/13
+
+int ln(double n){
+  double y,a,b,c; //y para guardar el ln de n 
+	a = n-1; 
+	b = n+1; 
+	c = a/b; 
+	y = a0*c+       //representacion de la serie de taylor
+	a1*c*c*c+ 
+	a2*c*c*c*c*c+ 
+	a3*c*c*c*c*c*c*c+ 
+	a4*c*c*c*c*c*c*c*c*c+ 
+	a5*c*c*c*c*c*c*c*c*c*c*c+ 
+	a6*c*c*c*c*c*c*c*c*c*c*c*c*c; 
+	return 2*y;
+} 
+    
 
 int main(){
   double numero;
