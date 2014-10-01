932620d4cb3fd6b4f91d6b58bc4fc097e8ebe159
PatoVargas <patricio.vargas@usach.cl>
2014-09-30 22:29:50 -0300

    Update ln.c
    cambiamos el scanf por la funcion getopt

--- ln.c
+++ ln.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+
+
 //Esta es la primera version de nuestro programa
 double ln(double n){
 	double y,a,b,c;			//y para guardar el ln de n
@@ -16,20 +21,47 @@ double ln(double n){
 	}
 	
 
-int main(){
-	double numero;
-	int i,iteraciones;
-	printf("ingrese un numero: ");
- 	scanf("%lf",&numero);
-	printf("ingrese cantidad de iteraciones: ");
- 	scanf("%i",&iteraciones);
-		
-	double a;
-	for (i=0; i < iteraciones; i++){     //ciclo for para realizar la cantidad de iteraciones que se ingresen
-		a = ln(numero);
+//funcion principal
+int main(int argc, char **argv){ 
+	extern char *optarg;			//arreglo definido por getopt
+	int opcion, error = 0;				// variables para el switch (opcion que se elige) y para un posible error respectivamente
+	int esta_i =0, esta_n=0;			// iniciamos variables para detectar si estan o no i y n en la linea de comando
+	int numero = 0, iteraciones =0,i;		//iniciamos las variables numero iteraciones e i
+	while((opcion = getopt(argc, argv, "i:n:"))!=-1) // while para recibir los argumento del argv
+		switch (opcion){			
+			case 'i':
+				//en el caso de encontrar una i en la linea de comando				
+				esta_i =1;	//registramos que había una i para las iteraciones
+				iteraciones = atoi(optarg);  //atoi para transformar la cadena de caracteres recibidos en un int y guardarlo en iteraciones
+				break;
+			case 'n':
+				//en el caso de encontrar una n en la linea de comando	
+				esta_n =1; 	//registramos que había un n para el numero
+				numero = atoi(optarg); //atoi para transformar la cadena de caracteres recibidos en un int y guardarlo en numero
+				break;
+			case '?':
+				// en otro caso 
+				error = 1;		//marcamos que hubo un error en la sintaxis de entrada
+				break;
+			
 	}
 
-	printf("el ln del numero es: %f\n", a);   //imprime resultado final del ln pedido
+	if (esta_i == 0){		// en caso de que falte el parametro iteraciones en la linea de comando
+		printf("falta ingresar iteraciones \n");
+	}
+	if (esta_n == 0){		// en caso de que falte n en la linea de comando
+		printf("falta ingresar numero \n");
 	}
 
 
+	if (error == 1){		// si encontramos algun error en la sintaxis
+		printf("error de sintaxis");	
+	}
+	
+	//para calcular el ln 
+	double a = 0; 			//iniciamos un double para guardar el numero
+	for(i=0;i<iteraciones;i++){ 	// for para repetir el proceso de calculo en iteraciones
+		a = ln3(numero); 	// llamamos a la funcion ln para el numero y la guardamos en a
+	} 
+	printf("%f\n", a); 		//devolver el valor del ln
+}
